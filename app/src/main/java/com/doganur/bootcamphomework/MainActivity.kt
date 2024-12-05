10765b6ffb2e8db24634137636b988bcf8adb8ef
doganuraydeniz <doganur.aydeniz@sekompos.com>
2024-12-05 14:17:06 +0300

    First bootcamp homework

---
+++ app/src/main/java/com/doganur/bootcamphomework/MainActivity.kt
@@ -0,0 +1,107 @@
+package com.doganur.bootcamphomework
+
+fun main() {
+    variables()
+}
+
+fun variables(){
+    val ilce = "Üsküdar"
+    val kita = "Anadolu"
+    val faks = "0212111111"
+    val postaKodu = "10440"
+    val instaAdresi = "yıldız_yıldız"
+    val calistiginBolum = "Software Development"
+    val urunMiktari = 99
+    val musteriSoyadi = "Aydeniz"
+    val odemeMiktari = 101
+    val dogumTarihi = "06-01-2001"
+    val borc = 902.9
+    val medeniHal = "Single"
+    val videoYorumu = "I like this video!"
+
+    val odemeSaati = "11:30"
+    val eftMiktari = 980.50
+    val satilanMiktar = 34
+    val telefonModeli = "iPhone 16"
+    val dergiAdi = "Chip"
+    val basimTarihi = "01-01-2001"
+    val zamMiktari = 23.000
+    val daireSayisi = 9
+    val enlem = 89.023
+    val boylam = 21.978
+    val yemekAdi = "Karniyarik"
+    val urunFiyati = 101.99
+
+    val sirket = "Doa Technology"
+    val videoAdi = "Learn Jetpack Compose"
+    val muzikSuresi = "04:45"
+    val mekanPuani = 4.2
+    val dosyaAdi = "homework1.kt"
+    val resimFormati = "PNG"
+    val renk = "Blue"
+    val renkKodu = "#0000FF"
+    val bilgisayarModeli = "MacBook Air"
+    val ekranBoyutu = 13.3
+    val kullanimSuresi = "2 years"
+
+    val basinc = 1013.25
+    val etkinlikGunu = "Friday"
+    val odemeGunu = "Sunday"
+    val yolculukCikisTarihi = "10-10-2024"
+    val mahalleAdi = "Maltepe"
+    val otobusHatti = "19S"
+    val kullanilanDakika = 26
+    val kargoTakipNo = "ABC123456"
+    val kuponSuresi = "30 days"
+    val kuponKodu = "SALE2024"
+    val faturaTarihi = "01-11-2024"
+
+    println(ilce)
+    println(kita)
+    println(faks)
+    println(postaKodu)
+    println(instaAdresi)
+    println(calistiginBolum)
+    println(urunMiktari)
+    println(musteriSoyadi)
+    println(odemeMiktari)
+    println(dogumTarihi)
+    println(borc)
+    println(medeniHal)
+    println(videoYorumu)
+
+    println(odemeSaati)
+    println(eftMiktari)
+    println(satilanMiktar)
+    println(telefonModeli)
+    println(dergiAdi)
+    println(basimTarihi)
+    println(zamMiktari)
+    println(daireSayisi)
+    println(enlem)
+    println(boylam)
+    println(yemekAdi)
+    println(urunFiyati)
+    println(sirket)
+    println(videoAdi)
+    println(muzikSuresi)
+    println(mekanPuani)
+    println(dosyaAdi)
+    println(resimFormati)
+    println(renk)
+    println(renkKodu)
+    println(bilgisayarModeli)
+    println(ekranBoyutu)
+    println(kullanimSuresi)
+    println(basinc)
+    println(etkinlikGunu)
+    println(odemeGunu)
+    println(yolculukCikisTarihi)
+    println(mahalleAdi)
+    println(otobusHatti)
+    println(kullanilanDakika)
+    println(kargoTakipNo)
+    println(kuponSuresi)
+    println(kuponKodu)
+    println(faturaTarihi)
+}
